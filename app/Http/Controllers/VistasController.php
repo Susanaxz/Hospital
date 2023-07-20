1e5b95a5e63df092f1dd2cfba827721a31171945
Susanaxz <riberasusana@gmail.com>
2023-07-20 16:12:54 +0200

    feat: logica del controlador de las vistas

--- app/Http/Controllers/VistasController.php
+++ app/Http/Controllers/VistasController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\PacienteController;
 
 class VistasController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function alta()
+    {
+        return view('alta');
+    }
+
+    public function consulta()
+    {
+        return redirect()->action([PacienteController::class, 'consultaPaciente']);
+    }
+
+    public function mantenimiento()
+    {
+        return view('mantenimiento');
+    }
 }
