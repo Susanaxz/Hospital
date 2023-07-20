7588c0b7d2300a164063ea43a40b3a4c0e8e5bcb
Susanaxz <riberasusana@gmail.com>
2023-07-20 12:45:30 +0200

    feat: creacion de archivos de modelo y controladores

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    public function altaPaciente(){
+        
+    }
+
+    public function consultaPaciente()
+    {
+    }
+
+    public function modificacionPaciente()
+    {
+    }
+
+    public function bajaPaciente()
+    {
+    }
+}
