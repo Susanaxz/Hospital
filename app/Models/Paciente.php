2fdb17edd7125181a562b6be356af75415a34421
Susanaxz <riberasusana@gmail.com>
2023-07-20 13:01:13 +0200

    feat: creacion del modelo de paciente

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Paciente extends Model
 {
     use HasFactory;
+
+    protected $table = 'paciente'; // nombre de la tabla
+    protected $primaryKey = 'idpaciente'; // id de la tabla paciente
+    public $timestamps = false; // no tiene timestamps
+
 }
