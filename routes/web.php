c906f0da27d6d8bcf1981b8b82dc2c5e128e6ba8
Susanaxz <riberasusana@gmail.com>
2023-07-25 13:10:49 +0200

    feat: añado la ruta para el login y cambio los nombres de las rutas

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,36 @@ use App\Http\Controllers\PacienteController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Ruta del login
+Route::get('/login', [VistasController::class, 'login'])->name('login');
+
 //Ruta raíz de entrada a la aplicación
-Route::get('/', [VistasController::class, 'home']);
+Route::get('/', [VistasController::class, 'home'])->name('home');
 
 // Ruta asociada a la opción de Alta Paciente del menú
-Route::get('/alta', [VistasController::class, 'alta']);
+Route::get('/alta', [VistasController::class, 'alta'])->name('alta');
 
 //Ruta asociada a la opción de Consulta Paciente del menú
-Route::get('/consulta', [VistasController::class, 'consulta']);
+Route::get('/consulta', [VistasController::class, 'consulta'])->name('consulta');
 
 // Ruta asociada a la opción de Baja/Modificación Paciente del menú
-Route::get('/mantenimiento', [VistasController::class, 'mantenimiento']);
+Route::get('/mantenimiento', [VistasController::class, 'mantenimiento'])->name('mantenimiento');
 
 //operativas de alta, consulta, modificación y baja de pacientes
 
 // Ruta asociada a la consulta de un paciente seleccionado en la vista que carga mantenimiento.blade.php
-Route::get('/paciente/{idpaciente}', [PacienteController::class, 'consultaPaciente']);
+Route::get('/paciente/{idpaciente}', [PacienteController::class, 'consultaPaciente'])->name('consultaPaciente');
 
 // Ruta asociada a la consulta de todos los pacientes
-Route::get('/paciente', [PacienteController::class, 'consultaPacientes']);
+Route::get('/paciente', [PacienteController::class, 'consultaPacientes'])->name('consultaPacientes');
 
 //Ruta asociada a la operativa de alta de paciente en la vista alta.blade.php
-Route::post('/paciente', [PacienteController::class, 'altaPaciente']);
+Route::post('/paciente', [PacienteController::class, 'altaPaciente'])->name('altaPaciente');
 
 //Ruta asociada a la operativa de modificación de paciente en la vista mantenimiento.blade.php
-Route::put('/paciente/{paciente?}', [PacienteController::class, 'modificacionPaciente']);
+Route::put('/paciente/{paciente?}', [PacienteController::class, 'modificacionPaciente'])->name('modificacionPaciente');
 
 //Ruta asociada a la operativa de baja de paciente en la vista mantenimiento.blade.php
-Route::delete('/paciente/{paciente?}', [PacienteController::class, 'bajaPaciente']);
+Route::delete('/paciente/{paciente?}', [PacienteController::class, 'bajaPaciente'])->name('bajaPaciente');
+
+
