031fe7c84bb7e5d7bb404f4e431f15ab59f33c31
Susanaxz <riberasusana@gmail.com>
2023-07-25 13:14:23 +0200

    feat: oculto de la vista los menus que solo se pueden acceder con autentificación

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Hospital</title>
+    <meta charset="UTF-8">
+    <title>Hospital</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/hospital.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/hospital.css') }}">
 </head>
 <body>
-<div class="container">
-	<header>
-		<h1 id="title">HOSPITAL</h1>
-	</header>
-    <div class='flex'>
-        <nav class="navbar bg-body-secondary">
-            <h3>Menu opciones:</h3>
-            <br>
-            <a class="navbar-brand" href="{{ url('consulta') }}">Consulta pacientes</a>
-            <a class="navbar-brand" href="{{ url('alta') }}">Alta paciente</a>
-            <a class="navbar-brand" href=" {{ url('mantenimiento') }} ">Baja/modificación paciente</a>
+    <div class="container">
+        <header>
+            <h1 id="title">HOSPITAL</h1>
+        </header>
+        <div class='flex'>
+            <nav class="navbar bg-body-secondary">
+                <h3>Menu opciones:</h3>
+                <br>
+                <a class="navbar-brand" href="{{ route('consulta') }}">Consulta pacientes</a>
 
 
-        </nav>
+                @auth
+                <a class="navbar-brand" href="{{ route('alta') }}">Alta paciente</a>
+                <a class="navbar-brand" href=" {{ route('mantenimiento') }} ">Baja/modificación paciente</a>
+                @endauth
 
-        <section id='contenido'>
-            <div>
-                @yield('contenido')
-            </div>
-        </section>
+
+            </nav>
+
+            <section id='contenido'>
+                <div>
+                    @yield('contenido')
+                </div>
+            </section>
+        </div>
     </div>
-</div>
 </body>
 </html>
+
